7a9b3f2568476172f1ca881ef5e6ae489f258223
Zayxc <141384673+Zayxc@users.noreply.github.com>
2024-11-11 03:50:07 +0700

    ztrialssh.sh

--- ztrialssh.sh
+++ ztrialssh.sh
@@ -14,19 +14,12 @@ LIGHT='\033[0;37m'
 
 clear
 IP=$(wget -qO- ipinfo.io/ip);
-ssl="$(cat ~/log-install.txt | grep -w "Stunnel5" | cut -d: -f2)"
-sqd="$(cat ~/log-install.txt | grep -w "Squid" | cut -d: -f2)"
 ovpn="$(netstat -nlpt | grep -i openvpn | grep -i 0.0.0.0 | awk '{print $4}' | cut -d: -f2)"
 ovpn2="$(netstat -nlpu | grep -i openvpn | grep -i 0.0.0.0 | awk '{print $4}' | cut -d: -f2)"
 Login=Trial`</dev/urandom tr -dc X-Z0-9 | head -c4`
 hari="1"
 Pass=1
 clear
-systemctl restart ws-tls
-systemctl restart ws-nontls
-systemctl restart ssh-ohp
-systemctl restart dropbear-ohp
-systemctl restart openvpn-ohp
 useradd -e `date -d "$masaaktif days" +"%Y-%m-%d"` -s /bin/false -M $Login
 exp="$(chage -l $Login | grep "Account expires" | awk -F": " '{print $2}')"
 hariini=`date -d "0 days" +"%Y-%m-%d"`
@@ -36,22 +29,10 @@ echo -e ""
 echo -e "Informasi Trial SSH & OpenVPN"
 echo -e "================================"
 echo -e "IP/Host       : $IP"
-echo -e "Domain        : $domain"
 echo -e "Username      : $Login"
 echo -e "Password      : $Pass"
-echo -e "OpenSSH       : 443, 22"
-echo -e "Dropbear      : 443, 109, 143"
-echo -e "SSL/TLS       :$ssl"
-echo -e "Port Squid    :$sqd"
-echo -e "OHP SSH       : 8181"
-echo -e "OHP Dropbear  : 8282"
-echo -e "OHP OpenVPN   : 8383"
-echo -e "Ssh Ws SSL    : 443"
-echo -e "Ssh Ws No SSL : 8880"
-echo -e "Ovpn Ws       : 2086"
 echo -e "Port TCP      : $ovpn"
 echo -e "Port UDP      : $ovpn2"
-echo -e "Port SSL      : 990"
 echo -e "OVPN TCP      : http://$IP:89/tcp.ovpn"
 echo -e "OVPN UDP      : http://$IP:89/udp.ovpn"
 echo -e "OVPN SSL      : http://$IP:89/ssl.ovpn"
@@ -67,4 +48,4 @@ echo -e "Payload Websocket No TLS"
 echo -e "=============================="
 echo -e "GET / HTTP/1.1[crlf]Host: ${domain}[crlf]Upgrade: websocket[crlf][crlf]"
 echo -e "=============================="
-echo -e "Script By Akbar Maulana"
+echo -e "Done"

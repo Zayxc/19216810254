253bad57975ec4bc19ce1cc6abd83436252bc7ba
Zayxc <141384673+Zayxc@users.noreply.github.com>
2024-11-11 03:40:32 +0700

    zopenmenu.sh

--- zopenmenu.sh
+++ zopenmenu.sh
@@ -5,12 +5,12 @@ y="\033[0;1;37m"
 yy="\033[0;1;32m"
 yl="\033[0;1;33m"
 wh="\033[0m"
-echo -e "$y                  MENU UTAMA $wh"
+echo -e "$y                  ZMENU UTAMA $wh"
 echo -e "$y-------------------------------------------------$wh"
-echo -e "$yy 1$y.  OpenVPN MENU  $wh"
+echo -e "$yy 1$y.  OpenVPN ZMENU  $wh"
 echo -e "$yy 13$y. Exit$wh"
 echo -e "$y-------------------------------------------------$wh"
-read -p "Select From Options [ 1 - 13 ] : " menu
+read -p "Select From Options [ 1 - 13 ] : " zmenu
 case $zmenu in
 1)
 clear

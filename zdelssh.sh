02703a37a396db94ff17b4132004a3edea35f064
Zayxc <141384673+Zayxc@users.noreply.github.com>
2024-11-13 03:25:17 +0700

    zdelssh.sh

--- zdelssh.sh
+++ zdelssh.sh
@@ -20,3 +20,9 @@ if getent passwd $Pengguna > /dev/null 2>&1; then
 else
         echo -e "Failure: Username $Pengguna Tidak Ada"
 fi
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo "";
+
+read -n 1 -s -r -p "Press any key to back on menu"
+
+zmenu

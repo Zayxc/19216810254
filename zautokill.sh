12dff37c0c5d07fcb5e13e7fb6b79da78c68eac1
Zayxc <141384673+Zayxc@users.noreply.github.com>
2024-11-13 03:23:55 +0700

    zautokill.sh

--- zautokill.sh
+++ zautokill.sh
@@ -102,3 +102,10 @@ case $AutoKill in
                 exit
                 ;;
         esac
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo "";
+
+read -n 1 -s -r -p "Press any key to back on menu"
+
+zmenu
+        

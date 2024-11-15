da4944dce8efa1198dd46f1b6153f48185f2af61
Zayxc <141384673+Zayxc@users.noreply.github.com>
2024-11-16 02:51:37 +0700

    openku3.sh

---
+++ openku3.sh
@@ -0,0 +1,140 @@
+#!/bin/bash
+# ==========================================
+# Color
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+# ==========================================
+# Getting
+MYIP=$(wget -qO- ipinfo.io/ip);
+# ==================================================
+apt update -y
+apt install curl -y
+apt install software-properties-common -y
+apt install -y nano
+apt install -y sudo
+sudo apt-get install wget screen
+
+
+# simple password minimal
+wget -O /etc/pam.d/common-password "https://raw.github.com/Zayxc/19216810254/main/password"
+chmod +x /etc/pam.d/common-password
+
+# go to root
+cd
+
+# Edit file /etc/systemd/system/rc-local.service
+cat > /etc/systemd/system/rc-local.service <<-END
+[Unit]
+Description=/etc/rc.local
+ConditionPathExists=/etc/rc.local
+[Service]
+Type=forking
+ExecStart=/etc/rc.local start
+TimeoutSec=0
+StandardOutput=tty
+RemainAfterExit=yes
+SysVStartPriority=99
+[Install]
+WantedBy=multi-user.target
+END
+
+# nano /etc/rc.local
+cat > /etc/rc.local <<-END
+#!/bin/sh -e
+# rc.local
+# By default this script does nothing.
+exit 0
+END
+
+# Ubah izin akses
+chmod +x /etc/rc.local
+
+# enable rc local
+systemctl enable rc-local
+systemctl start rc-local.service
+
+# disable ipv6
+echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6
+sed -i '$ i\echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6' /etc/rc.local
+
+#update
+apt update -y
+#apt upgrade -y
+#apt dist-upgrade -y
+#apt-get remove --purge ufw firewalld -y
+#apt-get remove --purge exim4 -y
+
+
+# install badvpn
+cd
+wget -O /usr/bin/badvpn-udpgw "https://raw.github.com/Zayxc/19216810254/main/badvpn-udpgw64"
+chmod +x /usr/bin/badvpn-udpgw
+sed -i '$ i\screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7100 --max-clients 500' /etc/rc.local
+sed -i '$ i\screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7200 --max-clients 500' /etc/rc.local
+sed -i '$ i\screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 500' /etc/rc.local
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7100 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7200 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7400 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7500 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7600 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7700 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7800 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7900 --max-clients 500
+
+
+[openvpn]
+accept = 990
+connect = 127.0.0.1:1194
+
+END
+
+[Install]
+WantedBy=multi-user.target
+END
+
+#OpenVPN
+wget https://raw.github.com/Zayxc/19216810254/main/zvpn.sh &&  chmod +x zvpn.sh && ./zvpn.sh
+
+
+# download script
+#cd /usr/bin
+#wget -O zmenu "https://raw.github.com/Zayxc/19216810254/main/zopenmenu.sh"
+#wget -O zsshovpnmenu "https://raw.github.com/Zayxc/19216810254/main/zmenuvpn.sh"
+#wget -O zaddssh "https://raw.github.com/Zayxc/19216810254/main/zaddssh.sh"
+#wget -O ztrialssh "https://raw.github.com/Zayxc/19216810254/main/ztrialssh.sh"
+#wget -O zdelssh "https://raw.github.com/Zayxc/19216810254/main/zdelssh.sh"
+#wget -O zmember "https://raw.github.com/Zayxc/19216810254/main/zmember.sh"
+#wget -O zdelexp "https://raw.github.com/Zayxc/19216810254/main/zdelexp.sh"
+#wget -O zcekssh "https://raw.github.com/Zayxc/19216810254/main/zcekssh.sh"
+#wget -O zceklim "https://raw.github.com/Zayxc/19216810254/main/zceklim.sh"
+#wget -O zrenewssh "https://raw.github.com/Zayxc/19216810254/main/zrenewssh.sh"
+#wget -O zautokill "https://raw.github.com/Zayxc/19216810254/main/zautokill.sh"
+#wget -O zclearlog "https://raw.github.com/Zayxc/19216810254/main/zclearlog.sh"
+
+#chmod +x zmenu
+#chmod +x zsshovpnmenu
+#chmod +x zaddssh
+#chmod +x ztrialssh
+#chmod +x zdelssh
+#chmod +x zmember
+#chmod +x zdelexp
+#chmod +x zcekssh
+#chmod +x zceklim
+#chmod +x zautokill
+#chmod +x zrenewssh
+#chmod +x zclearlog
+
+echo "0 5 * * * root clearlog && reboot" >> /etc/crontab
+echo "0 0 * * * root xp" >> /etc/crontab
+# remove unnecessary files
+
+
+# finihsing
+clear

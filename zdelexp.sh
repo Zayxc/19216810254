369287d13b8030e5b8b4da037de91ddfe11af5c4
Zayxc <141384673+Zayxc@users.noreply.github.com>
2025-01-27 23:04:51 +0700

    zdelexp.sh

--- zdelexp.sh
+++ zdelexp.sh
@@ -52,7 +52,6 @@ clear
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo "";
 
-read -n 1 -s -r -p "Press any key to back on menu"
-
+read -p "Kembali ke Menu tekan Enter"
 zmenu
 	

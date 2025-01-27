5f3d3a6f52a7631fa0f30d211f5efc91f2226c36
Zayxc <141384673+Zayxc@users.noreply.github.com>
2025-01-27 23:05:21 +0700

    zcekssh.sh

--- zcekssh.sh
+++ zcekssh.sh
@@ -84,7 +84,6 @@ done
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo "";
 
-read -n 1 -s -r -p "Press any key to back on menu"
-
+read -p "Kembali ke Menu tekan Enter"
 zmenu
 

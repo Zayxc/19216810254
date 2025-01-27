6d3c66fcba1e23ee3271303ffec7cdf369da8f02
Zayxc <141384673+Zayxc@users.noreply.github.com>
2025-01-27 23:51:29 +0700

    zrenewssh.sh

--- zrenewssh.sh
+++ zrenewssh.sh
@@ -47,6 +47,5 @@ fi
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo "";
 
-read -n 1 -s -r -p "Press any key to back on menu"
-
+read -p "Kembali ke Menu tekan Enter"
 zmenu
